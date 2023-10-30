106655ed7d8f9e67dd047697a77c249be3c13918
SaPradilla <andress_garces@soy.sena.edu.co>
2023-10-30 18:50:48 -0500

    cors and local mongo

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const app = express()
-const route = require('./routes')
-require('dotenv').config()
-const puerto = 7060
+    const express = require('express')
+    const mongoose = require('mongoose')
+    const bodyParser = require('body-parser')
+    const cors = require('cors')
+    const app = express()
+    const route = require('./routes')
+    require('dotenv').config()
+    const puerto = 7060
 
-// BD
-const uri = "mongodb+srv://pradi:sena123@testingapis.gxtyny0.mongodb.net/?retryWrites=true&w=majority"
+    // // BD
+    mongoose.set('strictQuery',true)
+    mongoose.Promise = global.Promise
+    mongoose.connect('mongodb://0.0.0.0:27017/restapi',{
+        useNewUrlParser:true
+    })
+    // const uri = "mongodb+srv://pradi4:sena123@forapis.ymzwneg.mongodb.net/api_clientes?retryWrites=true&w=majority"
+    
+    // mongoose.connect(uri,
+    //     { useNewUrlParser: true, useUnifiedTopology: true }
+    // )
+    // const db = mongoose.connection
+    // db.on("error",console.error.bind(console,"conection failed: "))
+    // db.once("open", function () {
+    //     console.log("Connect to the database successfully")
+    // })
+   
+    
+    // server
+    app.listen(puerto, () =>{
+        console.log(`Te has conectado a http://localhost:${puerto}`)
+    })
+    // capturar body
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-mongoose.connect(uri,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-).then(() =>
-    console.log('Base de datos conectada correctamente'
-    )).catch(error =>
-        console.log('error al conectarse a la db:', error
-    ))
+    // app.use(express.json());
 
-// server
-app.listen(puerto, () =>{
-    console.log(`Te has conectado a http://localhost:${puerto}`)
-})
-
-// capturar body
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// app.use(express.json());
-
-
-// rutas
-app.use('/',route)
+    app.use(cors())
+    // rutas
+    app.use('/',route)
